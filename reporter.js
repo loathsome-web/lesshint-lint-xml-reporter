6b11060f508cf39355a4a85e1d49f7a202b73738
loathsome <loathsome@yome.cz>
2020-05-29 19:12:49 +0200

    Fix(XML): checkstyle 4.3

--- reporter.js
+++ reporter.js
@@ -2,42 +2,50 @@
 
 'use strict';
 module.exports = {
-    name: 'lesshint-lint-xml-reporter',
-    report: function report (results) {
-        var resultsByfiles = {};
-        var output = '';
+  name: 'lesshint-lint-xml-reporter',
+  report: function report(results){
+    var resultsByfiles = {};
+    var output = '';
 
-        console.log('<?xml version="1.0" encoding="utf-8"?>');
-        console.log('<lint>');
+    console.log('<?xml version="1.0" encoding="utf-8"?>');
+    console.log('<checkstyle version="4.3">');
 
-        results.forEach(function (result) {
-            output = '\t<issue';
-            if (result.severity === 'error') {
-                output += ' severity="error"';
-            } else {
-                output += ' severity="warning"';
-            }
-            if (result.line) {
-                output += ' line="' + result.line + '"';
-            }
-            if (result.column) {
-                output += ' char="' + result.column + '"';
-            }
+    results.forEach(function(result){
+      output = '\t<error';
 
-            output += ' reason="' + result.linter + ': ' + result.message.replace(/"/g, '&quot;') + '"';
+      if(result.severity === 'error'){
+        output += ' severity="error"';
+      }
+      else{
+        output += ' severity="warning"';
+      }
 
-            if (resultsByfiles[result.file] === undefined) {
-                resultsByfiles[result.file] = '';
-            }
-            resultsByfiles[result.file] += output + ' />\n';
-        });
+      if(result.linter){
+        output += ' source="lesshint.rules.' + result.linter + '"';
+      }
 
-        Object.keys(resultsByfiles).forEach(function (file) {
-            console.log('<file name="' + file + '">');
-            console.log(resultsByfiles[file]);
-            console.log('</file>');
-        });
+      if(result.line){
+        output += ' line="' + result.line + '"';
+      }
 
-        console.log('</lint>');
-    }
+      if(result.column){
+        output += ' column="' + result.column + '"';
+      }
+
+      output += ' message="' + result.message.replace(/&/g, '&amp;').replace(/"/g, '&quot;') + '"';
+
+      if(resultsByfiles[result.fullPath] === undefined){
+        resultsByfiles[result.fullPath] = '';
+      }
+      resultsByfiles[result.fullPath] += output + ' />\n';
+    });
+
+    Object.keys(resultsByfiles).forEach(function(fullPath){
+      console.log('<file name="' + fullPath + '">');
+      console.log(resultsByfiles[fullPath]);
+      console.log('</file>');
+    });
+
+    console.log('</checkstyle>');
+  },
 };
